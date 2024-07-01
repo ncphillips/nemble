736195fd989111ed735bb48c6e1f0e99597364e0
nolan <nolan@ncphi.com>
2024-07-01 10:30:43 -0300

    Course aggregate has state

--- app/Aggregates/Course.php
+++ app/Aggregates/Course.php
@@ -8,6 +8,12 @@ use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
 
 class Course extends AggregateRoot
 {
+    public string $name;
+    public string $code;
+    public string $description;
+    public Carbon|null $startDate = null;
+    public Carbon|null $endDate = null;
+
     public function create(
         int         $createdByUserId,
         string      $name,
@@ -27,5 +33,14 @@ class Course extends AggregateRoot
         ));
 
         return $this;
+    }
+
+    protected function handleCourseCreated(CourseCreated $event)
+    {
+        $this->name = $event->name;
+        $this->code = $event->code;
+        $this->description = $event->description;
+        $this->startDate = $event->startDate;
+        $this->endDate = $event->endDate;
     }
 }
